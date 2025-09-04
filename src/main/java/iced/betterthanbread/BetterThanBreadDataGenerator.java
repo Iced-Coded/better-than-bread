08b25e825f26ef965b8677c89b0cb753b69dc25a
Iced-Coded <volkogon212@gmail.com>
2025-09-04 18:41:06 +0300

    Init

---
+++ src/main/java/iced/betterthanbread/BetterThanBreadDataGenerator.java
@@ -0,0 +1,11 @@
+package iced.betterthanbread;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterThanBreadDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
