b1a335e26d52ce6ab1c3418d148fcdc5a96a07c9
Iced-Coded <volkogon212@gmail.com>
2025-09-06 14:42:46 +0300

    new food classes

--- src/main/java/iced/betterthanbread/items/ModFoodComponent.java
+++ src/main/java/iced/betterthanbread/items/ModFoodComponent.java
@@ -10,31 +10,51 @@ public class ModFoodComponent {
     // Kids, don't eat raw dough
     public static FoodComponent RAW_CINNAMON_ROLL_DOUGH = new FoodComponent.Builder()
             .hunger(3)
-            .saturationModifier(0.75f)
+            .saturationModifier(0.35f)
             .statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 600), 1)
             .alwaysEdible()
             .build();
+
     public static FoodComponent RAW_CINNAMON_ROLL = new FoodComponent.Builder()
             .hunger(1)
             .saturationModifier(0.25f)
             .statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 600), 1)
             .alwaysEdible()
             .build();
+
     public static FoodComponent CINNAMON_ROLL = new FoodComponent.Builder()
             .hunger(4)
-            .saturationModifier(2)
+            .saturationModifier(0.70f)
             .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 300), 1)
             .build();
+
     public static FoodComponent GLAZED_CINNAMON_ROLL = new FoodComponent.Builder()
             .hunger(5)
-            .saturationModifier(2.5f)
+            .saturationModifier(0.75f)
             .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 300), 1)
             .build();
+
     // TODO: make sausage in dough and a beef in dough
-    public static FoodComponent SAUSAGE_IN_DOUGH = new FoodComponent.Builder()
+    public static FoodComponent MEAT_IN_DOUGH = new FoodComponent.Builder()
             .hunger(5)
+            .saturationModifier(0.75f)
             .build();
-    public static FoodComponent BEEF_IN_DOUGH = new FoodComponent.Builder()
-            .hunger(5)
+
+    public static FoodComponent RAW_MEAT_IN_DOUGH = new FoodComponent.Builder()
+            .hunger(2)
+            .saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 600), 1)
+            .build();
+
+    public static FoodComponent SAUSAGE = new FoodComponent.Builder()
+            .hunger(4)
+            .saturationModifier(0.5f)
+            .build();
+
+    public static FoodComponent RAW_SAUSAGE = new FoodComponent.Builder()
+            .hunger(2)
+            .saturationModifier(0.25f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA, 600), 1)
+            .statusEffect(new StatusEffectInstance(StatusEffects.POISON, 600), 1)
             .build();
 }
